858beac6369df7c4c348c43ef8864bb1701b3e26
rongx0915 <rongx0915@tamu.edu>
2021-11-04 12:55:20 -0500

    deploy test

--- testDeploy.py
+++ testDeploy.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, url_for
+import os
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+##### Pages #####
 @app.route("/")
 def home():
     return "Welcome to <h1>Nutritist</h1>"
@@ -13,6 +15,16 @@ def user(name):
 @app.route("/admin")
 def admin():
     return redirect(url_for("home"))
-    
+
+# @app.route('/<file_name>.txt')
+# def send_text_file(file_name):
+#     # testing for requirements.txt
+#     file_dot_text = file_name + '.txt'
+#     return app.send_static_file(file_dot_text)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Not found 404..."
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
